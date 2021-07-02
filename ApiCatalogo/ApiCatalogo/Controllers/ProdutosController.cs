95f1e37929c067602d1c7a80a7549117c1b88703
Yara Regina <yara.regina@hotmail.com>
2021-07-02 17:54:28 -0300

    Adicionando o ProdutosController

---
+++ ApiCatalogo/ApiCatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,30 @@
+﻿using ApiCatalogo.Context;
+using ApiCatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        //Injeção de dependencia
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        //Para acessar os dados da tabela
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+           return _context.Produtos.ToList();
+        }
+
+    }
+}
