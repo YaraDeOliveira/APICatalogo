4d912342b52aa559a7965af742cd140b1564ebaa
Yara Regina <yara.regina@hotmail.com>
2021-07-05 11:10:57 -0300

    Testando o tratamento de erros global

--- ApiCatalogo/ApiCatalogo/Startup.cs
+++ ApiCatalogo/ApiCatalogo/Startup.cs
@@ -1,4 +1,5 @@
 using ApiCatalogo.Context;
+using ApiCatalogo.Extensions;
 using ApiCatalogo.Filter;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -53,6 +54,10 @@ namespace ApiCatalogo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // adiciona o middleware de tratamento de erros
+            app.ConfigureExceptionHandler();
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
