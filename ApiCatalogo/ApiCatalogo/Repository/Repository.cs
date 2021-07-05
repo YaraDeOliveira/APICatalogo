81771efb76ba5094c845909c27b285a8a8ce0bbc
Yara Regina <yara.regina@hotmail.com>
2021-07-05 15:35:28 -0300

    Adicionando Repositorios ao projeto

---
+++ ApiCatalogo/ApiCatalogo/Repository/Repository.cs
@@ -0,0 +1,44 @@
+﻿using ApiCatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ApiCatalogo.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        // injeção de dependencia
+        protected AppDbContext _context;
+        public Repository(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+        // lista de entidades
+        // metodo set<T> do contexto retorna uma instancia Dbset<T> para o acesso a entidades de determinado tipo no contexto
+        public IQueryable<T> Get()
+        {
+            return _context.Set<T>().AsNoTracking();
+        }
+
+        public T GetById(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>().SingleOrDefault(predicate);
+        }
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.Set<T>().Update(entity);
+        }
+    }
+}
